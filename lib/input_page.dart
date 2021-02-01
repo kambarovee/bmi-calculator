568edbde8af3ea8ba58c5bc680c09b0f2c4e9ec8
Yerkebulan Kambarov <44515882+kambarovee@users.noreply.github.com>
2021-02-01 17:32:12 +0600

    Added some UI elements(Containers as a Card)

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,51 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(colour: Color(0xFF1D1E33)),
+                ReusableCard(colour: Color(0xFF1D1E33)),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(colour: Color(0xFF1D1E33)),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(colour: Color(0xFF1D1E33)),
+                ReusableCard(colour: Color(0xFF1D1E33)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }

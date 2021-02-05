5a88bb1a378cc0f53ed517f2b232a831278f1856
Yerkebulan Kambarov <44515882+kambarovee@users.noreply.github.com>
2021-02-05 19:37:15 +0600

    Added result page

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,6 +5,13 @@ const kInactiveCardBackgroundColor = Color(0xFF1d1f3b);
 const kButtonColor = Color(0xFFEB1555);
 const kInactiveButtonColor = Color(0xFF8E8D98);
 const kLabelsTextStyle = TextStyle(fontSize: 18.0, color: Color(0xFF8E8D98));
-const kNumbersTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
-const kCalculateTextStyle = TextStyle(fontSize: 35.0, fontWeight: FontWeight.w900);
-const kUnitTextStyle = TextStyle(fontSize: 28.0, color: Color(0xFF8E8D98),);
+const kNumbersTextStyle =
+    TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
+const kCalculateTextStyle =
+    TextStyle(fontSize: 35.0, fontWeight: FontWeight.w900);
+const kUnitTextStyle = TextStyle(
+  fontSize: 28.0,
+  color: Color(0xFF8E8D98),
+);
+const kResultTextStyle = TextStyle(
+    fontSize: 21.0, color: Color(0xFF24D876), fontWeight: FontWeight.bold);
--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,3 +1,5 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
@@ -7,7 +9,46 @@ class ResultPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Container(child: Text('Welcome!'),),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              child: Text(
+                'Your Result',
+                style: kCalculateTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardBackgroundColor,
+              widgetCard: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Normal',
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '22.7',
+                    style: kNumbersTextStyle,
+                  ),
+                  Text(
+                    'Normal BMI range:',
+                    style: kLabelsTextStyle,
+                  ),
+                  Text('18.5 - 25 kg/m^2'),
+                  Text('You have a normal body weight. Good job!')
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
